f64504560b156ffe408b57c96bd59c741fc137d1
Maryna Ishchenko <mishchenko@intellias.com>
2018-11-29 15:57:25 +0200

    Cars ready to review

--- week3/oop_cars/cars.py
+++ week3/oop_cars/cars.py
@@ -5,7 +5,7 @@ class CarBase:
     def __init__(self, brand, photo_file_name, carrying, car_type = None):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        self.carrying = float(carrying)
         self.car_type = None
 
     def get_photo_file_ext(self):
@@ -15,14 +15,17 @@ class CarBase:
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
-        self.passenger_seats_count = passenger_seats_count
+        self.passenger_seats_count = int(passenger_seats_count)
         self.car_type = 'car'
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying)
-        self.body_width, self.body_height, self.body_length = body_whl.split('x') or 0,0,0
+        if body_whl:
+            self.body_width, self.body_height, self.body_length = [float(x) for x in body_whl.split('x')]
+        else:
+            self.body_width, self.body_height, self.body_length = [0,0,0]
         self.car_type = 'truck'
 
     def get_body_volume(self):
@@ -45,14 +48,17 @@ def get_car_list(csv_filename):
                 if row[0] == 'car':
                     car_list.append(Car(row[1], row[3], row[5], row[2]))
                 elif row[0] == 'truck':
-                    car_list.append(Truck(row[1], row[3], float(row[5]), row[4]))
+                    car_list.append(Truck(row[1], row[3], row[5], row[4]))
                 elif row[0] == 'spec_machine':
-                    car_list.append(SpecMachine(row[1], row[3], float(row[5]), row[6]))
+                    car_list.append(SpecMachine(row[1], row[3], row[5], row[6]))
                 else:
                     pass
             except IndexError:
                 pass
     return car_list
 
+def _main():
+    get_car_list("coursera_week3_cars.csv")
 
-print(dir(get_car_list("coursera_week3_cars.csv")))
+if __name__ == "__main__":
+    _main()
